1b53c0637715522d05cdb22277b588d048c78d29
Gianni Leggio <giannileggio@gmail.com>
2016-05-26 07:51:58 +0100

    Style/RequestReferer autocorrect

--- spec/rubocop/cop/style/request_referer_spec.rb
+++ spec/rubocop/cop/style/request_referer_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe RuboCop::Cop::Style::RequestReferer, :config do
   subject(:cop) { described_class.new(config) }
 
-  context 'referer' do
+  context 'when EnforcedStyle is referer' do
     before { inspect_source(cop, 'puts request.referrer') }
     let(:cop_config) { { 'EnforcedStyle' => 'referer' } }
     it 'registers an offense for request.referrer' do
@@ -20,9 +20,14 @@ describe RuboCop::Cop::Style::RequestReferer, :config do
       expect(cop.messages)
         .to eq(['Use `request.referer` instead of `request.referrer`'])
     end
+
+    it 'autocorrects referrer with referer' do
+      corrected = autocorrect_source(cop, ['puts request.referrer'])
+      expect(corrected).to eq 'puts request.referer'
+    end
   end
 
-  context 'referrer' do
+  context 'when EnforcedStyle is referrer' do
     before { inspect_source(cop, 'puts request.referer') }
     let(:cop_config) { { 'EnforcedStyle' => 'referrer' } }
     it 'registers an offense for request.referer' do
@@ -36,6 +41,11 @@ describe RuboCop::Cop::Style::RequestReferer, :config do
     it 'sends a message to the user' do
       expect(cop.messages)
         .to eq(['Use `request.referrer` instead of `request.referer`'])
+    end
+
+    it 'autocorrects referer with referrer' do
+      corrected = autocorrect_source(cop, ['puts request.referer'])
+      expect(corrected).to eq 'puts request.referrer'
     end
   end
 end
